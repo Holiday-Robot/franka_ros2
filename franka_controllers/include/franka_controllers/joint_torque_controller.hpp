6dabab07e8a6a75bdc09a85b9f89abb853dfeceb
jaemin.yoon <jaemin.yoon@holiday-robotics.com>
2025-01-07 16:51:29 +0900

    add joint pos/vel/torque controller based on ROS2

---
+++ franka_controllers/include/franka_controllers/joint_torque_controller.hpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2023 Franka Robotics GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <string>
+
+
+#include <Eigen/Eigen>
+#include <controller_interface/controller_interface.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include "franka_semantic_components/franka_robot_state.hpp"
+#include "rclcpp/subscription.hpp"
+#include "realtime_tools/realtime_buffer.hpp"
+#include "realtime_tools/realtime_publisher.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace franka_controllers {
+
+class JointTorqueController : public controller_interface::ControllerInterface {
+ public:
+  [[nodiscard]] controller_interface::InterfaceConfiguration command_interface_configuration()
+      const override;
+  [[nodiscard]] controller_interface::InterfaceConfiguration state_interface_configuration()
+      const override;
+  controller_interface::return_type update(const rclcpp::Time& time,
+                                           const rclcpp::Duration& period) override;
+  CallbackReturn on_init() override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
+
+ private:
+  std::string arm_id_;
+  std::string robot_description_;
+  const int num_joints = 7;
+
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_command_subscriber_ =
+    nullptr;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
+  std::unique_ptr<realtime_tools::RealtimePublisher<sensor_msgs::msg::JointState>> joint_state_publisher_;
+
+  void joint_command_callback(const std::shared_ptr<sensor_msgs::msg::JointState> msg);
+  void publish_state(const sensor_msgs::msg::JointState & joint_state);
+  bool validate_effort_msg(const sensor_msgs::msg::JointState & command) const;
+  void updateJointStates();
+
+  realtime_tools::RealtimeBuffer<std::shared_ptr<sensor_msgs::msg::JointState>>
+    joint_command_msg_external_point_ptr_;
+
+  sensor_msgs::msg::JointState joint_state;
+
+
+};
+
+}  // namespace franka_controllers
---
+++ franka_controllers/src/joint_velocity_controller.cpp
@@ -0,0 +1,223 @@
+// Copyright (c) 2023 Franka Robotics GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <franka_controllers/default_robot_behavior_utils.hpp>
+#include <franka_controllers/joint_velocity_controller.hpp>
+#include <franka_controllers/robot_utils.hpp>
+
+#include <cassert>
+#include <cmath>
+#include <exception>
+#include <string>
+
+#include <Eigen/Eigen>
+
+using namespace std::chrono_literals;
+
+namespace franka_controllers {
+
+controller_interface::InterfaceConfiguration
+JointVelocityController::command_interface_configuration() const {
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  for (int i = 1; i <= num_joints; ++i) {
+    config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/velocity");
+  }
+  return config;
+}
+
+controller_interface::InterfaceConfiguration
+JointVelocityController::state_interface_configuration() const {
+  controller_interface::InterfaceConfiguration config;
+  config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  for (int i = 1; i <= num_joints; ++i) {
+    config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/position");
+    config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/velocity");
+    config.names.push_back(arm_id_ + "_joint" + std::to_string(i) + "/effort");
+  }
+  return config;
+}
+
+controller_interface::return_type JointVelocityController::update(
+    const rclcpp::Time& /*time*/,
+    const rclcpp::Duration& /*period*/) {
+
+  auto joint_command_msg = joint_command_msg_external_point_ptr_.readFromRT();
+
+  if (joint_command_msg){
+    std::shared_ptr<sensor_msgs::msg::JointState> joint_command = *joint_command_msg;
+    for (int i = 0; i < num_joints; ++i) {
+      command_interfaces_[i].set_value(joint_command->velocity[i]);
+    }
+  }
+
+  updateJointStates();
+  publish_state(joint_state);
+
+  return controller_interface::return_type::OK;
+}
+
+CallbackReturn JointVelocityController::on_init() {
+  try {
+    auto_declare<std::string>("robot_description", "");
+  } catch (const std::exception& e) {
+    fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+    return CallbackReturn::ERROR;
+  }
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn JointVelocityController::on_configure(
+    const rclcpp_lifecycle::State& /*previous_state*/) {
+  auto parameters_client =
+      std::make_shared<rclcpp::AsyncParametersClient>(get_node(), "/robot_state_publisher");
+  parameters_client->wait_for_service();
+
+  auto future = parameters_client->get_parameters({"robot_description"});
+  auto result = future.get();
+  if (!result.empty()) {
+    robot_description_ = result[0].value_to_string();
+  } else {
+    RCLCPP_ERROR(get_node()->get_logger(), "Failed to get robot_description parameter.");
+  }
+
+  arm_id_ = robot_utils::getRobotNameFromDescription(robot_description_, get_node()->get_logger());
+
+  auto client = get_node()->create_client<franka_msgs::srv::SetFullCollisionBehavior>(
+      "service_server/set_full_collision_behavior");
+  auto request = DefaultRobotBehavior::getDefaultCollisionBehaviorRequest();
+
+  auto future_result = client->async_send_request(request);
+  future_result.wait_for(1000ms);
+
+  auto success = future_result.get();
+  if (!success) {
+    RCLCPP_FATAL(get_node()->get_logger(), "Failed to set default collision behavior.");
+    return CallbackReturn::ERROR;
+  } else {
+    RCLCPP_INFO(get_node()->get_logger(), "Default collision behavior set.");
+  }
+
+
+  // subscribe
+  joint_command_subscriber_ =
+    get_node()->create_subscription<sensor_msgs::msg::JointState>(
+      "/hday/controller/joint_command", rclcpp::SystemDefaultsQoS(),
+      std::bind(&JointVelocityController::joint_command_callback, this, std::placeholders::_1));
+
+  // publish
+  publisher_ =
+    get_node()->create_publisher<sensor_msgs::msg::JointState>("/hday/controller/joint_state", rclcpp::SystemDefaultsQoS());
+  joint_state_publisher_ = std::make_unique<realtime_tools::RealtimePublisher<sensor_msgs::msg::JointState>>(publisher_);
+
+  joint_state_publisher_->lock();
+  joint_state_publisher_->msg_.name.resize(num_joints);
+  joint_state_publisher_->msg_.position.resize(num_joints);
+  joint_state_publisher_->msg_.velocity.resize(num_joints);
+  joint_state_publisher_->msg_.effort.resize(num_joints);
+  joint_state_publisher_->unlock();
+
+  joint_state.header.stamp = get_node()->now();
+  joint_state.name.resize(num_joints);
+  joint_state.position.resize(num_joints);
+  joint_state.velocity.resize(num_joints);
+  joint_state.effort.resize(num_joints);
+
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn JointVelocityController::on_activate(
+    const rclcpp_lifecycle::State& /*previous_state*/) {
+  updateJointStates();
+  return CallbackReturn::SUCCESS;
+}
+
+void JointVelocityController::joint_command_callback(
+  const std::shared_ptr<sensor_msgs::msg::JointState> msg)
+{
+  if (!validate_velocity_msg(*msg))
+  {
+    return;
+  }
+  joint_command_msg_external_point_ptr_.writeFromNonRT(msg);
+};
+
+
+bool JointVelocityController::validate_velocity_msg(
+  const sensor_msgs::msg::JointState & command) const
+{
+  if (command.name.empty())
+  {
+    RCLCPP_ERROR(get_node()->get_logger(), "Empty joint names on incoming command.");
+    return false;
+  }
+
+  if (command.name.size() != num_joints)
+  {
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+      "Joint name size mismatch");
+    return false;
+  }
+
+  if (command.velocity.size() != num_joints)
+  {
+    RCLCPP_ERROR(
+      get_node()->get_logger(),
+      "Velocity command size mismatch");
+    return false;
+  }
+
+  return true;
+}
+
+void JointVelocityController::updateJointStates() {
+  joint_state.header.stamp = get_node()->now();
+  for (auto i = 0; i < num_joints; ++i) {
+    const auto& position_interface = state_interfaces_.at(3 * i);
+    const auto& velocity_interface = state_interfaces_.at(3 * i + 1);
+    const auto& effort_interface = state_interfaces_.at(3 * i + 2);
+
+    assert(position_interface.get_interface_name() == "position");
+    assert(velocity_interface.get_interface_name() == "velocity");
+    assert(effort_interface.get_interface_name() == "effort");
+
+    joint_state.position[i] = position_interface.get_value();
+    joint_state.velocity[i] = velocity_interface.get_value();
+    joint_state.effort[i] = effort_interface.get_value();
+  }
+}
+
+
+void JointVelocityController::publish_state(
+  const sensor_msgs::msg::JointState & joint_state)
+{
+  if (joint_state_publisher_ && joint_state_publisher_->trylock())
+  {
+    joint_state_publisher_->msg_.header.stamp = joint_state.header.stamp;
+    for (int i = 1; i <= num_joints; ++i) {
+      joint_state_publisher_->msg_.name[i] = arm_id_ + "_joint" + std::to_string(i);
+    }
+    joint_state_publisher_->msg_.position = joint_state.position;
+    joint_state_publisher_->msg_.velocity = joint_state.velocity;
+    joint_state_publisher_->msg_.effort = joint_state.effort;
+  }
+  joint_state_publisher_->unlockAndPublish();
+}
+
+}  // namespace franka_controllers
+#include "pluginlib/class_list_macros.hpp"
+// NOLINTNEXTLINE
+PLUGINLIB_EXPORT_CLASS(franka_controllers::JointVelocityController,
+                       controller_interface::ControllerInterface)
